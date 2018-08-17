d5f4f97e5634bb67980768dfd2a47b50f5bb3b8c
Leonardo Souza <leogsouza@gmail.com>
2018-08-17 17:40:54 -0300

    Add control buttons

--- ch01-introduction/pomodoro-timer/index.js
+++ ch01-introduction/pomodoro-timer/index.js
@@ -1,7 +1,13 @@
-const POMODORO_STATE = {
+const POMODORO_STATES = {
   WORK: 'work',
   REST: 'rest'
 };
+
+const STATES = {
+  STARTED: 'started',
+  STOPPED: 'stopped',
+  PAUSED: 'paused'
+}
 
 const WORKING_TIME_LENGTH_IN_MINUTES = 25;
 const RESTING_TIME_LENGTH_IN_MINUTES = 5;
@@ -9,15 +15,48 @@ const RESTING_TIME_LENGTH_IN_MINUTES = 5;
 new Vue({
   el: '#app',
   data: {
+    state: STATES.STOPPED,
     minute: WORKING_TIME_LENGTH_IN_MINUTES,
     second: 0,
-    pomodoroState: POMODORO_STATE.WORK,
+    pomodoroState: POMODORO_STATES.WORK,
     timestamp: 0
+  },
+  computed: {
+    title: function () {
+      return this.pomodoroState === POMODORO_STATES.WORK 
+        ? 'Work!'
+        : 'Rest!'
+    },
+    min: function () {
+      if (this.minute < 10) {
+        return '0' + this.minute;
+      }
+
+      return this.minute;
+    },
+    sec: function () {
+      if (this.second < 10) {
+        return '0' + this.second;
+      }
+      return this.second;
+    }
   },
   methods: {
     start: function() {
+      this.state = STATES.STARTED;
       this._tick();
-      this.internal = setInterval(this._tick, 1000);
+      this.interval = setInterval(this._tick, 1000);
+    },
+    pause: function() {
+      this.state = STATES.PAUSED;
+      clearInterval(this.interval);
+    },
+    stop: function() {
+      this.state = STATES.STOPPED;
+      clearInterval(this.interval);
+      this.pomodoroState = POMODORO_STATES.WORK;
+      this.minute = WORKING_TIME_LENGTH_IN_MINUTES;
+      this.second = 0;
     },
     _tick: function() {
       // if second is not 0, just derement second
@@ -35,11 +74,11 @@ new Vue({
 
       // if second is 0 and minute is 0,
       // toggle working/resting intervals
-      this.pomodoroState = this.pomodoroState === POMODORO_STATE.WORK 
-        ? POMODORO_STATE.REST
-        : POMODORO_STATE.WORK;
+      this.pomodoroState = this.pomodoroState === POMODORO_STATES.WORK 
+        ? POMODORO_STATES.REST
+        : POMODORO_STATES.WORK;
       
-      if (this.pomodoroState === POMODORO_STATE.WORK) {
+      if (this.pomodoroState === POMODORO_STATES.WORK) {
         this.minute = WORKING_TIME_LENGTH_IN_MINUTES;
       } else {
         this.minute = RESTING_TIME_LENGTH_IN_MINUTES;
