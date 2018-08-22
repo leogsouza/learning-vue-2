5f66b45c0e053b3523caeabb22a05b4d852c829b
Leonardo Souza <leogsouza@gmail.com>
2018-08-22 11:10:55 -0300

    Add more math functions

--- ch-02-fundamentals/square/VueMathPlugin.js
+++ ch-02-fundamentals/square/VueMathPlugin.js
@@ -5,6 +5,16 @@ export default {
     });
     Vue.directive('sqrt', function(el, binding) {
       el.innerHTML = Math.sqrt(binding.value);
+    });
+    Vue.directive('sine', function(el, binding) {
+      el.innerHTML = Math.sin(binding.value);
+    });
+    Vue.directive('cosine', function(el, binding) {
+      el.innerHTML = Math.sqrt(binding.value);
+    });
+    Vue.directive('tangent', function(el, binding) {
+      el.innerHTML = Math.tan(binding.value);
     })
+
   }
 };
