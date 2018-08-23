ae064cb11cdfd41f177c72858871318c259f843f
Leonardo Souza <leogsouza@gmail.com>
2018-08-23 11:06:09 -0300

    Scaffolding shopping list app

---
+++ ch-02-fundamentals/shopping-list/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app"  class="container">
+  <h2>{{ title }}</h2>
+    <add-item-component></add-item-component>
+    <items-component></items-component>
+    <div class="footer">
+      <hr>
+      <em>Change the title of your shopping list here</em>
+      <change-title-component></change-title-component>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
